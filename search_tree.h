78dcaae00dcba614b5c11538baa9688c45bfbbd8
lxy0401 <403824215@qq.com>
2018-05-15 20:44:02 +0800

    搜索二叉树

--- search_tree.h
+++ search_tree.h
@@ -8,3 +8,15 @@ typedef struct SearchNode
 }SearchNode;
 
 
+//初始化
+void SearchTreeInit(SearchNode** pRoot);
+
+//销毁
+void SearchTreeDestroy(SearchNode* root);
+void SearchTreeDestroy2(SearchNode** pRoot);
+
+//向搜索树中插入元素
+SearchNode* CreateSearchNode(SearchNodeType value);
+
+//递归的方式查找元素
+SearchNode* SearchTreeFind(SearchNode* root,SearchNodeType to_find);
